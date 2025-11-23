79686b8c4d8650132c3713253c1c42a4ed7eb749
Natalia Pelenitsyna <natalipele94@gmail.com>
2025-11-23 23:49:21 +0800

    Шаг 1-2

---
+++ tests/app.spec.js
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test'
+
+test('app renders login form on start', async ({ page }) => {
+  await page.goto('/');
+
+  // Ищем поля формы авторизации
+  const usernameInput = page.getByLabel(/username/i);
+  const passwordInput = page.getByLabel(/password/i);
+  const signInButton = page.getByRole('button', { name: /sign in/i });
+
+  // Проверяем, что элементы видимы
+  await expect(usernameInput).toBeVisible();
+  await expect(passwordInput).toBeVisible();
+  await expect(signInButton).toBeVisible();
+})
